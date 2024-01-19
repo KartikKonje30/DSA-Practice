71b961225f49264cc76c4992a9cfa7a5b72d62c7
KartikKonje30 <kartikkonje.01@gmail.com>
2024-01-19 11:40:06 +0530

    1st commit

---
+++ Basic Math Problem/math.js
@@ -0,0 +1,26 @@
+//To take input from user via console
+
+
+
+
+// function count_digit(n){
+//     1st Approach using ready functions
+//     let str = n.toString();
+//     return str.length
+
+//     2nd Approach by dividing the number and setting a count variable.
+//     let count = 0;
+//     while(n!=0){
+//         n=Math.floor(n/10);
+//         count++;
+//     }
+//     return count;
+
+//     3rd Approach by using log10() and then adding 1.
+//     As The number of digits in an integer = the upper bound of log10(n).
+//     let count = Math.floor(Math.log10(n) + 1);
+//     return count;
+// }
+
+// console.log(count_digit(1234))
+
