33a2754ae5f88556ae7ea8b4f754a29c682501a2
KartikKonje30 <kartikkonje.01@gmail.com>
2025-05-01 12:54:10 +0530

    Operators start

--- Java/Basics/Datatypes.java
+++ Java/Basics/Datatypes.java
@@ -1,8 +1,12 @@
-package Java.Basics;
+
+import java.util.*;
 
 public class Datatypes {
 
     public static void main(String[] args){
+
+        // Variable, Datatypes and Type Conversion
+        // video: https://www.youtube.com/watch?v=g3WDfu14rpk&list=TLPQMDEwNTIwMjUYNEHwVT5Bcw&index=1
         
         /*
          * Datatypes in Java:
@@ -48,6 +52,41 @@ public class Datatypes {
          *      Used to store a single character/letter or ASCII value
          * 
          */
+
+         int num = 12;
+        //  float floatNum = 0.32; 
+         /* Lossy conversion as java recommends you to use double instead of float so as to store more digits but you can specify it by writing 'f' after the value to let java know that i accpet it as float and don't care of the extra space instead */
+
+         float floatNum = 0.32f;
+        
+         double doubleNum = 0.32; /* Here it won't recommend you as double has sufficient space to store a big fraction value */
+         
+         long longNum = 241435;
+
+         char chaValue = '$';
+
+         byte byteNum = 120;
+
+         short shortNum = 12421;
+
+         /* You can type convert from one datatype to another */
+
+        //  int integerNum = 24;
+        //  long longNum2 = integerNum;
+
+         // This is easy to convert but a bit different when it comes to converting from big size datatype to a small one
+
+            // long LongNum2 = 2418;
+            // int integrNum = LongNum2; 
+            // Again it shows lossy conversion as it have enough space in long store so why to store it again in a datatype having smaller space
+            // But if such situations occurs when you need to perform explicit type conversion, you can simply let java know by mentioning type while assigning variable
+            // It is also known as casting.
+            // It is done manually by programers. If we do not perform casting then java compilr returns an compile-time error.
+
+            long LongNum2 = 2418;
+            int integerNum = (int)LongNum2; 
+
+            System.out.println(integerNum); 
 
     }
     
---
+++ Java/Basics/Operators.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Operators {
+
+    public static void main(String[] args){
+        
+        // Java Operators, Binary number system and taking input from user
+        // video: https://www.youtube.com/watch?v=Xi1fx9QidWE&list=TLPQMDEwNTIwMjUYNEHwVT5Bcw&index=2
+
+        
+    }
+    
+}
