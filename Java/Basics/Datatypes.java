b0ef71f2ce52f654569c1814ca99038bb52e4493
KartikKonje30 <kartikkonje.01@gmail.com>
2025-05-01 11:59:30 +0530

    basic in java

---
+++ Java/Basics/Datatypes.java
@@ -0,0 +1,54 @@
+package Java.Basics;
+
+public class Datatypes {
+
+    public static void main(String[] args){
+        
+        /*
+         * Datatypes in Java:
+         * 
+         *  1. boolean: 
+         *      Default: false
+         *      Size: 1 bit
+         *      Used to store true or false
+         * 
+         *  2. byte:
+         *      Default: 0
+         *      Size: 1 byte / 8 bits
+         *      Used to store values ranging from -128 to 127 ( -2^7 to 2^7-1 )
+         * 
+         *  3. short:
+         *      Default: 0
+         *      Size: 2 bytes / 16 bits
+         *      Used to store values ranging from -32768 to 32767 ( -2^15 to 2^15-1)
+         *     
+         *  4. int:
+         *      Default: 0
+         *      Size: 4 bytes / 32 bits
+         *      Used to store values ranging from -2147483648 to 2147483647 ( -2^31 to 2^31-1)
+         * 
+         *  5. float:
+         *      Default: 0.0f
+         *      Size: 4 bytes / 32 bits
+         *      Usefull for storing fractions, sufficient for storing 6 to 7 decimal digits only
+         * 
+         *  6. long:
+         *      Default: 0L
+         *      Size: 8 bytes / 64 bits
+         *      Used to store values ranging from -9223372036854775808 to 9223372036854775807 ( -2^63 to 2^63-1)
+         * 
+         *  7. double:
+         *      Default: 0.0d
+         *      Size: 8 bytes / 64 bits
+         *      Usefull for storing fractions, sufficient for storing 15 decimal digits
+         * 
+         *  8. char:
+         *      Default: \u0000
+         *      Size: 2 bytes / 16 bits
+         *      Used to store a single character/letter or ASCII value
+         * 
+         */
+
+    }
+    
+}
--- Java/String/StringTest.java
+++ Java/String/StringTest.java
@@ -1,8 +1,19 @@
+
+import java.util.*;
+
 public class StringTest{
 
     public static void main(String[] args)
     {
-        System.out.println("Hello World!");
+        String name;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter your name: ");
+        name = sc.nextLine(); 
+
+
+        System.out.println("Hello "+ name);
     }
 
 }
