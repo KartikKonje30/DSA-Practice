8160f3e5ae17de55f204deb3592ecdd88e60a6bd
KartikKonje30 <kartikkonje.01@gmail.com>
2025-05-08 12:41:49 +0530

    Operators Complete

--- Java/Basics/Operators.java
+++ Java/Basics/Operators.java
@@ -1,4 +1,3 @@
-import java.util.*;
 
 public class Operators {
 
@@ -7,7 +6,68 @@ public class Operators {
         // Java Operators, Binary number system and taking input from user
         // video: https://www.youtube.com/watch?v=Xi1fx9QidWE&list=TLPQMDEwNTIwMjUYNEHwVT5Bcw&index=2
 
+        // Operators are placed in between operands to calculate 
+
+        // Arithmetic Operators:
+        // +: Addition, 
+        // -: Subtraction, 
+        // *: Multiplication, 
+        // /: Division ( Returns Quotient ), 
+        // %: Modulos ( Returns Remainder)
+
+        int a = 12;
+        int b = 13;
+
+        int c = a+b;
+
+        double d = (double)a/b; 
+        // When performing actions on int variable all the calculations are done based on int so we need to explicitly typecast one variable to convert the result into double
         
+        // System.out.println(d);
+
+        // Assignment Operators:
+        // =: a = b
+        // +=: a = a + b
+        // -=: a = a - b
+        // *=: a = a * b
+        // /=: a = a / b
+        // %=: a = a % b
+
+
+        // Relational Operators: Returns boolean true or false
+        // ==: a == b
+        // !=: a != b
+        // >: a > b
+        // <: a < b
+        // >=: a >= b
+        // <=: a <= b
+
+        // System.out.println(a != b);
+
+        // Logical Operators: Returns boolean true or false
+        // && [AND]: expression_1 && expression_2
+        // || [OR]: expression_1 || expression_2
+        // ! [NOT]: !expression_1
+
+        // System.out.println( (a == b) || (a > b) );
+
+        //Bitwise Operators: Advanced Operators used for Bit Manipulation
+        // ~: Bitwise Complement
+        // <<: Left Shift
+        // >>: Right Shift
+        // >>>: Unsigned Right Shift
+        // &: Bitwise AND
+        // ^: Bitwise OR
+
+
+        // Increment / Decremental Operators:
+        // ++: a = a + 1;
+        // --: a = a - 1;
+
+        // Ternary Operators: used to check conditions just like if-else condition 
+        // ? 
+        // :
+
     }
     
 }
